953bee6bf4376b8d17a0ff71d80edba22f8bcb17
Suvigya Agrawal <suvigya.agrawal@workindia.in>
2021-06-22 19:42:32 +0530

    Added ListProductsController with limit and page number acceptable in query params

--- api/controllers.py
+++ api/controllers.py
@@ -1,9 +1,46 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from rest_framework import status
 
 from api.adapters import ShopifyAdminApiAdapter
 from api.exceptions import ShopifyProductAdditionException
-from api.models import OrderItem
+from api.models import OrderItem, Product
 from api.serializers import ShopifyProductsSerializer, ShopifyCreateOrderSerializer
+
+
+class ListProductsController:
+    def __init__(self, request):
+        self.request = request
+        self.params = request.GET
+        self.response = None
+        self.api_response_status = None
+        print("ListProductsController init")
+
+    def process(self):
+        limit = self.params.get("limit", 10)
+        page = int(self.params.get("page", 1))
+        products_list = Product.objects.all()
+        paginator = Paginator(products_list, limit)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            page = 1
+            products = paginator.page(page)
+        except EmptyPage:
+            page = paginator.num_pages
+            products = paginator.page(paginator.num_pages)
+
+        serialized_products = []
+        for product in products:
+            serializer = ShopifyProductsSerializer(product)
+            serialized_products.append(serializer.data)
+
+        self.response = {
+            "count": paginator.count,
+            "current_page": page,
+            "total_pages": paginator.num_pages,
+            "products": serialized_products,
+        }
+        return self.response, status.HTTP_200_OK
 
 
 class ShopifyGetProductsController:
