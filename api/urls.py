607498c0199683d865a440d1983cec02f4694f5e
Suvigya Agrawal <suvigya.agrawal@workindia.in>
2021-06-22 19:10:53 +0530

    Added create_order url and view

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r"^health$", views.health_view, name="health"),
     url(r"^shopify/get_products$", views.shopify_get_products_view, name="shopify_get_products"),
+    url(r"^shopify/create_order", views.shopify_create_order_view, name="shopify_create_order"),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.adapters import ShopifyAdminApiAdapter
-from api.controllers import ShopifyGetProductsController
+from api.controllers import ShopifyGetProductsController, ShopifyCreateOrderController
 from api.serializers import ShopifyProductsSerializer
 
 
@@ -18,5 +18,11 @@ def health_view(request):
 
 @api_view(['GET'])
 def shopify_get_products_view(request):
-    response = ShopifyGetProductsController(request).process()
-    return Response(data=response, status=status.HTTP_200_OK)
+    response, status_code = ShopifyGetProductsController(request).process()
+    return Response(data=response, status=status_code)
+
+
+@api_view(['POST'])
+def shopify_create_order_view(request):
+    response, status_code = ShopifyCreateOrderController(request).process()
+    return Response(data=response, status=status_code)
