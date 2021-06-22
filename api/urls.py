f77d377a770aada7610233fc3a1a77cf6e068250
Suvigya Agrawal <suvigya.agrawal@workindia.in>
2021-06-22 14:44:54 +0530

    Added endpoint to call get-products

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r"^health$", views.health_view, name="health"),
+    url(r"^shopify/get_products$", views.shopify_get_products_view, name="shopify_get_products"),
 ]
--- api/views.py
+++ api/views.py
@@ -5,8 +5,19 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from api.adapters import ShopifyAdminApiAdapter
+
 
 @api_view(['GET'])
 def health_view(request):
     print("Health")
     return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def shopify_get_products_view(request):
+    print("shopify_get_products_view")
+    import ipdb;
+    ipdb.set_trace()
+    response = ShopifyAdminApiAdapter().get_products()
+    return Response(data=response, status=status.HTTP_200_OK)
