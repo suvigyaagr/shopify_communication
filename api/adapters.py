b9a1520238ac65bf1e5dd5a3e810c5d5d463ba8a
Suvigya Agrawal <suvigya.agrawal@workindia.in>
2021-06-22 14:34:39 +0530

    Added ShopifyAdminApiAdapter and get_products

---
+++ api/adapters.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+
+SHOPIFY_BASE_API_URL = 'https://a38f4a6a8cb713fe2bebdbf3df331f54:3182dcd29ff6c3f6f2dd325ba99b4216@mishipaytestdevelopmentemptystore.myshopify.com'
+
+
+class ShopifyAdminApiAdapter:
+    def __init__(self):
+        self.base_url = SHOPIFY_BASE_API_URL
+
+    def get_products(self):
+        try:
+            request = requests.get(
+                url=f'{self.base_url}/admin/api/2021-01/products.json',
+            )
+            if request.status_code == 200:
+                response = json.loads(request.text)
+                print(response)
+                return response
+
+        except Exception as e:
+            print(e)
+        return None
