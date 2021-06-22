a3ca22c2069f71c3d847a168301a8879c77d951f
Suvigya Agrawal <suvigya.agrawal@workindia.in>
2021-06-22 17:24:05 +0530

    Registered Product and Variant in Admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Product, Variant
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    pass
